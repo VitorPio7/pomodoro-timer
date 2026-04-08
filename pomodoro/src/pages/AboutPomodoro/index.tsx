9378d78eca85981da63d77b587b07d6c16733e3a
vitorpio7 <vitorvpio60@gmail.com>
2026-04-08 17:28:47 -0300

    feat:creating the file

--- pomodoro/src/pages/AboutPomodoro/index.tsx
+++ pomodoro/src/pages/AboutPomodoro/index.tsx
@@ -23,7 +23,7 @@ export function AboutPomodoro() {
             garantir descansos para evitar o cansaço mental.
           </p>
 
-          <img src='https://placehold.co/1920x1080' alt='' />
+          <img  src='studies.jpg' alt='' />
 
           <h2>Como funciona o Pomodoro tradicional?</h2>
           <ul>
